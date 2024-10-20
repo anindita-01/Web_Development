9f7e6da1164bc3b3d3098f59204ddfc1d77bf3f4
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2024-10-20 13:38:44 +0530

    Fix:fixed missing code fragments & add: enable dark mode feature

--- txtutils/src/App.js
+++ txtutils/src/App.js
@@ -1,31 +1,38 @@
+import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+// import About from './components/About';
 
- 
+
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const [mode, setMode] = useState('light');
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      // showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      // showAlert("Light mode has been enabled", "success");
     }
   }
+
   return (
-    <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below" mode={mode}/>
-    </div>
-    </> 
+     <>
+
+<Navbar title="Txtutils" about="About Us" mode={mode} toggleMode={toggleMode}/>    
+<div className="container">
+<TextForm heading="Enter your text" mode={mode}/>
+{/* <About/> */}
+</div>
+
+    </>
   );
 }
 
--- txtutils/src/components/TextForm.js
+++ txtutils/src/components/TextForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function TextForm({ heading }) {
+export default function TextForm({ heading,mode }) {
   const handleUpClick = () => {
     // console.log("Uppercase was click");
     let newText = text.toUpperCase();
@@ -12,6 +12,17 @@ export default function TextForm({ heading }) {
     let newText = text.toLowerCase();
     setText(newText);
   };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text); 
+//     props.showAlert("Copied to Clipboard!", "success");
+ }
+
+const handleExtraSpaces = () => {
+  let newText = text.split(/[ ]+/);
+  setText(newText.join(" "));
+  // props.showAlert("Extra spaces removed!", "success");
+}
 
   const handleClearClick = () => {
     // console.log("Uppercase was click");
@@ -36,12 +47,13 @@ export default function TextForm({ heading }) {
   // setText("new text")
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color: mode==='dark'?'white':'#042743'}}>
         <div className="my-3">
           <h1>{heading}</h1>
           <textarea
             className="form-control my-4"
             id="myBox1"
+            style={{backgroundColor: mode==='dark'?'#13466e':'white', color: mode==='dark'?'white':'#042743'}}
             value={text}
             onChange={handleOnChange}
             rows="8"
@@ -58,17 +70,31 @@ export default function TextForm({ heading }) {
           <button className="btn btn-primary mx-1" onClick={handleCapitalize}>
             Capitalize text
           </button>
+          <button
+            // disabled={text.length === 0}
+            className="btn btn-primary mx-2"
+            onClick={handleCopy}
+          >
+            Copy Text
+          </button>
+          <button
+            // disabled={text.length === 0}
+            className="btn btn-primary mx-2 "
+            onClick={handleExtraSpaces}
+          >
+            Remove Extra Spaces
+          </button>
         </div>
       </div>
 
-      <div className="container my-3">
+      <div className="container my-3" style={{color: mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
         <p>
           {text.split(" ").length} words, {text.length} characters
         </p>
         <p>{0.08 * text.split(" ").length} Minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Nothing to preview!"}</p>
       </div>
     </>
   );
