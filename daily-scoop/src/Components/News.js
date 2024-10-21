c94c9eeb0b428edf6a47ca2469679d5718f9ed13
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2024-10-22 01:06:00 +0530

    setup: initial setup; add: bootstrap; navbar,news and newsitems components

---
+++ daily-scoop/src/Components/News.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        this is a news  Component
+        <NewsItems/>
+        
+        
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ daily-scoop/src/Components/NewsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        this is a news item Component
+      </div>
+    )
+  }
+}
+
+export default NewsItems
