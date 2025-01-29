2b22bf27250c1397eefc438d75f027a632f7dc3c
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-01-29 21:59:46 +0530

    wip: setting tailwind

--- Dashboard/dashboard/src/App.jsx
+++ Dashboard/dashboard/src/App.jsx
@@ -1,16 +1,16 @@
-
-
+import { Dashboard } from "./Components/Dashboard"
 
 function App() {
  
   return (
     <>
-    <div className="text-3xl font-bold underline">  
-    Hello motherfucker!!!
-    </div>
-     
+    
+    <Dashboard/>
     </>
+   
   )
+   
+  
 }
 
 export default App
--- Dashboard/dashboard/tailwind.config.js
+++ Dashboard/dashboard/tailwind.config.js
@@ -2,8 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      borderWidth: {
+        1: "1 px",
+      },
+      fontFamily: {
+        Quicksand: ["Quicksand", serif],
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      }
+    },
   },
   plugins: [],
 };
 
+<span>
+  <Dashboard>
+      
+
+
+
+  </Dashboard>
+</span>
