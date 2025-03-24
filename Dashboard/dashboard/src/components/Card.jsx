0d227620c3db99a6815bd8dbc54ff3f6c785e825
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-03-24 22:40:10 +0530

    amazon clone

--- Dashboard/dashboard/src/components/Card.jsx
+++ Dashboard/dashboard/src/components/Card.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Card=({children})=> {
   return (
+    <>
     <div className='w-full h-full rounded-md relative p-8 border-2 bg-gray-300'>
       {children}
     </div>
+
+
+    
+    </>
+    
   );
 };
 
