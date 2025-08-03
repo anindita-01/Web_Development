8b18ca8430e496d9e3d4c8e71254af618c95c5b4
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-08-03 23:21:57 +0530

    add: functionality

--- iChat/js/client.js
+++ iChat/js/client.js
@@ -1,8 +1,38 @@
-const socket= io("http://localhost:8000");
+const socket = io("http://localhost:8000");
 
-const form= document.getElementById('send-container')
-const messegeInput= document.getElementById('messageInp')
-const messegeContainer= document.querySelector('container')
+const form = document.getElementById("send-container");
+const messegeInput = document.getElementById("messageInp");
+const messegeContainer = document.querySelector(".container");
+var audio = new Audio("ting.mp3");
 
-const userName = prompt("Enter your userName to join");
+const append = (messege, position) => {
+  const messegeElement = document.createElement("div");
+  messegeElement.innerText = messege;
+  messegeElement.classList.add("messege");
+  messegeElement.classList.add(position);
+  messegeContainer.append(messegeElement);
+  if (position == "left") audio.play();
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messegeInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messegeInput.value = "";
+});
+
+// const userName = prompt("Enter your userName to join");
 socket.emit("new-user-joined", userName);
+
+socket.on("user-joined", (userName) => {
+  append(`${userName} joined the chat`, "right");
+});
+
+socket.on("receive", (data) => {
+  append(`${data.userName} : ${data.messege}`, "left");
+});
+
+socket.on("left", (userName) => {
+  append(`${userName} left the chat`, "right");
+});
--- iChat/nodeServer/index.js
+++ iChat/nodeServer/index.js
@@ -1,19 +1,27 @@
 // nodeserver which will handle socket.io connections
 
-const io = require("socket.io")(8000);
+const http = require("http").createServer();
+const io = require("socket.io")(http, {
+  cors: { origin: "*" }, // Optional, helpful if testing with frontend
+});
+http.listen(8000, () => console.log("Server running on port 8000"));
 
 const users = {};
 
 io.on("connection", (socket) => {
-  socket.on("new-user-joined", userName => {
-    console.log("new user", userName)
+  socket.on("new-user-joined", (userName) => {
+    // console.log("new user", userName);
     users[socket.id] = userName;
     socket.broadcast.emit("user-joined", userName);
   });
   socket.on("send", (messege) => {
-    socket.broadcast.emit("receivr", {
+    socket.broadcast.emit("receive", {
       messege: messege,
       userName: users[socket.id],
     });
+  });
+  socket.on("disconnect", (messege) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
   });
 });
